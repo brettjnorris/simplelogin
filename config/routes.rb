3d5b036de22ce9f1a03e5ee79eb1d41f41c51bfa
Brett J Norris <brettjnorris@gmail.com>
2016-02-12 13:15:29 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
+  get "/pages/:page" => "pages#show"
 end
