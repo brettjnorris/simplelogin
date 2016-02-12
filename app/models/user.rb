d1165b70bc6bdf38ddf34ded4acedbdfeace8517
Brett J Norris <brettjnorris@gmail.com>
2016-02-11 22:25:38 -0500

    Add devise to model; add user routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
   validates :email, presence: true, uniqueness: true
   validates :phone, presence: true, numericality: true, length: { is: 10 }
 end
